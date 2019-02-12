8b2fc3c7b6d47ffc4e8207de5d95ff405345655e
O’Hearon, KG <KG.Ohearon@t4g.com>
2019-02-12 12:03:36 -0400

    working on client. Very basic.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,36 @@
+import { Query } from "react-apollo";
 import React from "react";
+import gql from "graphql-tag";
 
-const App = () => <div> Hello World </div>;
+const App = () => (
+  <Query query={GET_USERS_QUERY}>
+    {({ data: { users }, error, loading }) => {
+      if (loading) {
+        return <div>Loading</div>;
+      }
+      if (error) {
+        return <div> Erro</div>;
+      }
+      return (
+        <div>
+          <ul>
+            {users.map(user => (
+              <li key={user.id}> {user.name} </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }}
+  </Query>
+);
 
 export default App;
+
+const GET_USERS_QUERY = gql`
+  query GET_USERS_QUERY {
+    users {
+      id
+      name
+    }
+  }
+`;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,20 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
+
+import { ApolloProvider } from "react-apollo";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
