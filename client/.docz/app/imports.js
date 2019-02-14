b75becbaac7f8ec93a094d32f93e5acc33d07dff
O’Hearon, KG <KG.Ohearon@t4g.com>
2019-02-13 23:44:13 -0400

    working on styled input.

---
+++ client/.docz/app/imports.js
@@ -0,0 +1,4 @@
+export const imports = {
+  'src/components/FormTextInput/FormTextInput.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-form-text-input-form-text-input" */ 'src/components/FormTextInput/FormTextInput.mdx'),
+}
--- client/src/components/FormTextInput/FormTextInput.jsx
+++ client/src/components/FormTextInput/FormTextInput.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 
 import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+const StyledInput = styled.input`
+  border-radius: 4px;
+  padding: 10px 20px;
+  border: 1px solid #c4c4c4;
+  font-family: "Roboto", sans-serif;
+  font-size: 14px;
+  transition: all 0.2s ease-in-out;
+
+  &:focus {
+    outline: none;
+    border: 2px solid #0b5fff;
+    padding: 9px 19px;
+  }
+`;
 
 const FormTextInput = ({ placeholder }) => {
   const [value, setValue] = useState("");
@@ -9,7 +25,11 @@ const FormTextInput = ({ placeholder }) => {
     setValue(e.target.value);
   };
   return (
-    <input placeholder={placeholder} value={value} onChange={handleChange} />
+    <StyledInput
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+    />
   );
 };
 
