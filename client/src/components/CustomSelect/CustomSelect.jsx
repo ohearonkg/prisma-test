bc7c8cda5397d4747d6510836890df455e19395d
O’Hearon, KG <KG.Ohearon@t4g.com>
2019-02-15 13:13:59 -0400

    using babel jest to compile tests.

--- client/src/components/CustomSelect/CustomSelect.jsx
+++ client/src/components/CustomSelect/CustomSelect.jsx
@@ -1,7 +1,34 @@
-import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 
-const CustomSelect = ({ options }) => <div>{options[0]} </div>;
+import PropTypes from "prop-types";
+
+const CustomSelect = ({ options }) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [open, setOpen] = useState(false);
+
+  const handleSelect = option => {
+    setSelectedOption(option);
+    setOpen(false);
+  };
+
+  return (
+    <div aria-label="custom-select" onClick={() => setOpen(true)}>
+      {/* Showing the currently selected option */}
+      <div>{selectedOption}</div>
+
+      {/* Showing all options*/}
+      {open && (
+        <div>
+          {options.map((option, index) => (
+            <div onClick={() => handleSelect(option)} key={index}>
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 CustomSelect.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired
--- client/src/components/CustomSelect/CustomSelect.spec.jsx
+++ client/src/components/CustomSelect/CustomSelect.spec.jsx
@@ -3,14 +3,46 @@ import { cleanup, fireEvent, render } from "react-testing-library";
 import CustomSelect from "./CustomSelect";
 import React from "react";
 
-afterEach(cleanup);
-
 const sampleOptions = ["A", "B", "C"];
 const setup = () => render(<CustomSelect options={sampleOptions} />);
 
+afterEach(cleanup);
+
 describe("Custom Select", () => {
+  /**
+   * First value on render
+   */
   it("Should render the first value upon initial render", () => {
     const { getByText } = setup();
     expect(getByText(sampleOptions[0]));
+  });
+
+  /**
+   * Showing all values when clicked
+   */
+  it("Should show all possible values passed to the component's options prop when the user clicks on the select", () => {
+    const { getByLabelText, getByText } = setup();
+    fireEvent.click(getByLabelText("custom-select"));
+    sampleOptions.forEach(option => expect(getByText(option)));
+  });
+
+  /**
+   * Should close when the user makes a selection
+   */
+  it("Should close when the user makes a selection", async () => {
+    const { queryAllByText, getByLabelText, getByText } = setup();
+    fireEvent.click(getByLabelText("custom-select"));
+    fireEvent.click(getByText(sampleOptions[1]));
+    await expect(queryAllByText(sampleOptions[0])).toBeNull();
+  });
+
+  /**
+   * Should display the selected value
+   */
+  it("Should display the selected value", () => {
+    const { getByLabelText, getByText } = setup();
+    fireEvent.click(getByLabelText("custom-select"));
+    fireEvent.click(getByText(sampleOptions[1]));
+    expect(getByText(sampleOptions[1]));
   });
 });
